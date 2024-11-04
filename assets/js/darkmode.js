6a8b2943a130ca979642a050c4b6c349709dfd17
kalan <rkaalaan@gmail.com>
2024-11-04 15:46:09 +0330

    Changes in dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -7,12 +7,32 @@ document.querySelector('.enable-dark-mode').addEventListener('click', function()
 document.querySelector('.disable-dark-mode').addEventListener('click', function() {
     document.documentElement.classList.remove('dark');
 });
-document.querySelector('.enable-dark-mode-mobile').addEventListener('click', function() {
-    document.documentElement.classList.add('dark'); 
+// document.querySelector('.enable-dark-mode-mobile').addEventListener('click', function() {
+//     document.documentElement.classList.add('dark'); 
+// });
+
+// document.querySelector('.disable-dark-mode-mobile').addEventListener('click', function() {
+//     document.documentElement.classList.remove('dark');
+// });
+ 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const switchElement = document.getElementById('darkSwitch');
+
+    switchElement.addEventListener('change', function() {
+        if (this.checked) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    });
 });
 
-document.querySelector('.disable-dark-mode-mobile').addEventListener('click', function() {
-    document.documentElement.classList.remove('dark');
-});
+
+
+
+
+
+
 
 
