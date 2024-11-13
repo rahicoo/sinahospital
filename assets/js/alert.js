1b5574e3dfc89e26c63d6a3aa71b63a45578e222
kalan <rkaalaan@gmail.com>
2024-11-13 10:25:31 +0330

    changes in dropdown

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,10 +1,33 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
+//     document.getElementById('badge').addEventListener('click', function() {
+//         const dropdown = document.getElementById('dropdownContent');
+//         dropdown.classList.toggle('hidden'); 
+//     });
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('badge').addEventListener('click', function() {
-        const dropdown = document.getElementById('dropdownContent');
-        dropdown.classList.toggle('hidden'); 
+      const dropdown = document.getElementById('dropdownContent');
+  
+      if (dropdown.classList.contains('hidden')) {
+        dropdown.classList.remove('hidden');
+        setTimeout(() => {
+          dropdown.classList.add('opacity-100');
+          dropdown.classList.remove('opacity-0');
+        }, 10);
+      } else {
+        dropdown.classList.add('opacity-0');
+        dropdown.classList.remove('opacity-100');
+        setTimeout(() => {
+          dropdown.classList.add('hidden');
+        }, 500);
+      }
     });
-});
+  });
+  
 
 
